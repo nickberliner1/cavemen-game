ab2e49dee33baa6e12dfca90071bcf8f570973c6
Nick Berliner <40582488+nickberliner1@users.noreply.github.com>
2020-09-11 17:08:47 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,22 +4,40 @@ import { Weapon } from "./weapons.js";
 
 
 class App {
-    constructor(){
-        this.map = new Map(8, 8);
-        this.players = [new Player("Red Caveman", "cavemanRed", 1, 100, null), 
-                        new Player("Blue Caveman", "cavemanBlue", 2, 100, null)];
-        
-        this.weapons = [new Weapon("Rock", "rock", 10),
-                        new Weapon("Stick", "stick", 20),
-                        new Weapon("Spear", "spear", 30),
-                        new Weapon("Mammoth", "mammoth", 40)];
+    constructor(players){
+        this.map = new Map(10, 10);
+        // give them default weapons
+        this.players = players;
+
+        this.weapons = [
+                        new Weapon("Fire", "fire", 20),
+                        new Weapon("Stick", "stick", 30),
+                        new Weapon("Spear", "spear", 40),
+                        new Weapon("Mammoth", "mammoth", 50)];
     }
 
     start(){
         this.map.generate(this.players, this.weapons);
+        this.map.displayPlayerMoves(this.players[0], this.players[1]);
+        
     }
 }
 
+const playerOne = new Player("Red Caveman", "cavemanRed", 1, 100, new Weapon("Rock", "rock", 10));
+const playerTwo = new Player("Blue Caveman", "cavemanBlue", 2, 100, new Weapon("Rock", "rock", 10));
+        
 
-let app = new App();
+let app = new App([playerOne, playerTwo]);
 app.start();
+
+
+$('cavemanRedDefend').on("click", () => {
+    playerOne.defend = true;
+})
+
+$('cavemanBlueDefend').on("click", () => {
+    playerTwo.defend = true;
+})
+
+window.app = app;
+
