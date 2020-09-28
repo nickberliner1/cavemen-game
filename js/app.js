493856c85ad24c28ed7ffba0bab570e907c25f32
Nick Berliner <40582488+nickberliner1@users.noreply.github.com>
2020-09-28 18:35:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,8 +9,7 @@ class App {
         // give them default weapons
         this.players = players;
 
-        this.weapons = [
-                        new Weapon("Fire", "fire", 20),
+        this.weapons = [new Weapon("Fire", "fire", 20),
                         new Weapon("Stick", "stick", 30),
                         new Weapon("Spear", "spear", 40),
                         new Weapon("Mammoth", "mammoth", 50)];
@@ -21,23 +20,119 @@ class App {
         this.map.displayPlayerMoves(this.players[0], this.players[1]);
         
     }
+
 }
 
-const playerOne = new Player("Red Caveman", "cavemanRed", 1, 100, new Weapon("Rock", "rock", 10));
-const playerTwo = new Player("Blue Caveman", "cavemanBlue", 2, 100, new Weapon("Rock", "rock", 10));
-        
-
+export const playerOne = new Player("Red Caveman", "cavemanRed", 1, 100, new Weapon("Rock", "rock", 10));
+export const playerTwo = new Player("Blue Caveman", "cavemanBlue", 2, 100, new Weapon("Rock", "rock", 10));
+      
 let app = new App([playerOne, playerTwo]);
 app.start();
 
+export const players = {
+    activePlayer: playerOne,
+    listOfPlayers: [playerOne, playerTwo],
 
-$('cavemanRedDefend').on("click", () => {
-    playerOne.defend = true;
+    changeActivePlayers() {
+        const inactivePlayer = this.listOfPlayers.pop();
+        this.listOfPlayers.unshift(inactivePlayer);
+
+        // Highlights border of active player
+        $(`#${this.activePlayer.playerPicture}Controller`).removeClass(`${this.activePlayer.playerPicture}BoardColor`);
+        $(`#${inactivePlayer.playerPicture}Controller`).addClass(`${inactivePlayer.playerPicture}BoardColor`);
+
+        // Changes board color to active player's color
+        $('#board').removeClass(`${this.activePlayer.playerPicture}BoardColor`);
+        $('#board').addClass(`${inactivePlayer.playerPicture}BoardColor`);
+        
+        this.activePlayer = inactivePlayer;
+    },
+
+    showHealth() {
+        $('#playerOneHealth').html('Health: ' + Math.max(playerOne.health, 0));
+        $('#playerTwoHealth').html('Health: ' + Math.max(playerTwo.health, 0));
+
+        // Checks if a player is dead
+        if ( playerOne.health <= 0 ) {
+            if ( confirm('Blue Caveman wins. Do you want to play again?') ) {
+                location.reload();
+            }            
+        }
+        if ( playerTwo.health <= 0 ) {
+            if ( confirm('Red Caveman wins. Do you want to play again?') ) {
+                location.reload();
+            }
+        }
+    }
+    
+}
+
+
+
+
+// Function that changes players urn after clicking either attack or defend
+$('button').on("click", () => {
+    players.changeActivePlayers();
+    showActivePlayerButtons();
 })
 
-$('cavemanBlueDefend').on("click", () => {
-    playerTwo.defend = true;
-})
+// attacking and defending buttons
+$('.cavemanRedAttack').on("click", () => {
+    attack(playerOne);
+});
+
+$('.cavemanRedDefend').on("click", () => {
+    defend(playerOne);
+});
+
+$('.cavemanBlueAttack').on("click", () => {
+    attack(playerTwo);
+});
+
+$('.cavemanBlueDefend').on("click", () => {
+    defend(playerTwo);
+});
+
+// Hides and shows attack and defend buttons for active player
+export function showActivePlayerButtons() {
+    $('#cavemanRedAttackAndDefendContainer').css('visibility', 'hidden');
+    $('#cavemanBlueAttackAndDefendContainer').css('visibility', 'hidden');
+    $(`#${players.activePlayer.playerPicture}AttackAndDefendContainer`).css('visibility', 'visible');
+}
+
+function attack(player) {
+    player.action = "attack";
+    calculateDamage();
+}
+
+function defend(player) {
+    player.action = "defend";
+    calculateDamage();
+}
+
+function calculateDamage() {
+    if ( playerOne.action && playerTwo.action ) {
+        if ( playerOne.action == "attack" ) {
+            if ( playerTwo.action == "attack" ) {
+                // if both players chose attack
+                playerOne.health -= playerTwo.weapon.damage;
+                playerTwo.health -= playerOne.weapon.damage;
+            } else {
+                // if player 2 chose defend
+                playerTwo.health -= playerOne.weapon.damage / 2;
+            }
+        } else {
+            //if player 1 chose defend
+            if ( playerTwo.action == "attack" ) {
+                playerOne.health -= playerTwo.weapon.damage / 2;
+            }
+        }
+        
+        players.showHealth();
+        // reset actions
+        playerOne.action = null;
+        playerTwo.action = null;
+    }
+}
 
 window.app = app;
-
